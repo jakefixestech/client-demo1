4ab7bff2e09a7dd10643b3425686d41d349d9f75
Jake <jake@jakefixestech.com>
2026-06-14 17:59:36 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
